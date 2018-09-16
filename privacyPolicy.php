9762e326072a51e89c21efd09972e611893d9ac9
RMIV2 <L00119067@student.lyit.ie>
2018-09-16 18:59:29 +0100

    Privacy Policy Page

---
+++ privacyPolicy.php
@@ -0,0 +1,69 @@
+<?php include "header.php";?>
+
+	<!-- USER Policy -->
+
+	<div class="userPolicy">
+		<div class="container">
+			<div class="row">
+
+				<div class="col">
+					<div class="section_title_container text-center">
+						<h2 class="section_title"><br><br>User Policy</h2><br>
+						<div class="section_subtitle">
+							<h4 align="left"><strong>This Page Contains the DevOps IT Data Protection Policy for Staff and Students</strong></h4><br>
+							<p align="justify">On 25 May 2018, new EU legislation came into force - the General Data Protection Regulations (GDPR) - applying to all data relating to, and descriptive of, living individuals defined in the Regulations as “personal data”.  
+												Individuals are referred to as “data subjects”.<br>
+												<br>
+												In undertaking the business of Letterkenny Institute of Technology (LYIT), we all create, gather, store and process large amounts of data on a variety of data subjects such as on students (both potential, current and former), 
+												staff, third parties and members of the public.  
+												Our use of personal data ranges from CCTV footage, financial transactions with commercial customers through to the processing a student’s details throughout their journey, from application through to graduation.
+												<br><br>
+												The GDPR places obligations on LYIT and the way it handles personal data.  In turn, the staff and students of the Institute have responsibilities to ensure personal data is processed fairly, lawfully and securely.  
+												This means that personal data should only be processed if we have a valid condition of processing (e.g. consent obtained from the data subject, or a contract with them, etc) 
+												and we have provided information to the individuals concerned about how and why we are processing their information (i.e. a privacy notice).  
+												There are restrictions on what we are allowed to do with personal data such as passing personal information on to third parties, transferring information outside the EU or using it for direct marketing.
+												<br><br>
+												LYIT is committed to a policy of protecting the rights and freedoms of individuals with respect to the processing of their personal data.
+												<br><br>
+												LYIT has developed a Privacy Notice which outlines how it deals with a student’s information that must be adhered to in order to comply with GDPR.  
+												LYIT has also developed a Privacy Notice which outlines how it deals with information gathered when subjects are using the LYIT website.  
+												<br><br>
+												LYIT has established the high-level principles relating to Data Protection in order to comply with GDPR requirements, and these can be viewed here
+												<br><br>
+												Further Information
+												LYIT in meeting its data privacy commitments have appointed a Data Protection Officer (DPO) as the point of contact for all data privacy queries that you may have, including subject access requests. 
+												You can contact the Data Protection Officer at:
+												<br><br><br>
+												Data Protection Officer<br>
+												Letterkenny Institute of Technology<br>
+												Port Road<br>
+												Letterkenny<br>
+												Co Donegal<br>
+												F92 FC93<br>
+												<br>
+												T:  +353 74 918 6021<br>
+												E:  dpo@lyit.ie<br>
+								
+						</div>
+					</div>
+				</div>
+
+				<!-- User Policy Main Content -->
+				<div class="col-lg-12">
+					<div class="userPolicy">
+						<div class="row courses_row">
+
+								
+						
+
+						</div>
+					</div>
+				</div>
+
+				
+			</div>
+		</div>
+	</div>
+
+<?php include "footer.php";?>
+<script src="js/userPolicy.js"></script>
