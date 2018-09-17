ac21063f496dcd1b965dfb1a609ea22bd495b71a
L00113835 <mtwmcg1@gmail.com>
2018-09-17 19:36:10 +0100

    Updated registration pages

--- registerStudent.php
+++ registerStudent.php
@@ -1,6 +1,6 @@
-@ Author: Mary Walsh McGinty
+<!-- @ Author: Mary Walsh McGinty
 @ Date: 2/9/18
-@ Desc: A PHP file to add registration data to the database
+@ Desc: A PHP file to add registration data to the database -->
 <?php include "header.php";?>
 <?php
  $server="localhost";
@@ -9,33 +9,53 @@
  $link=mysqli_connect($server,$dbuser,$password);
  mysqli_select_db($link,"regdb");
 
-    $fname=$_POST["Fname"];
-    $lname=$_POST["Lname"];
-    $add1=$_POST["add1"];
-    $add2=$_POST["add2"];
+    $Fname=$_POST["Fname"];
+    $Lname=$_POST["Lname"];
+    $street=$_POST["street"];
+    $village=$_POST["village"];
     $town=$_POST["town"];
     $county=$_POST["county"];
     $email=$_POST["email"];
     $phone=$_POST["phone"];
-    $dob=$_POST["DOB"];
-    $image=$_POST["Image"];
-   
-    $sql_insert ="INSERT INTO studentdetails(Fname,Lname,add1,add2,town,county,email,phone,DOB,Image)
-    VALUES('$fname','$lname','$add1','$add2','$town','$county','$email','$phone','$dob','$image')";
+    $dob=$_POST["dob"];
+    $pic=$_POST["pic"];
+    $passwd=$_POST["passwd"];
+$sql_insert1="INSERT INTO student (Fname,Lname,street,village,town,county,email,phone,dob,pic)
+ VALUES('$Fname','$Lname','$street','$village','$town','$county','$email','$phone','$dob','$pic')";
 
-	$passwd=$_POST["passwd"];
-	$sql_insert= "INSERT INTO user (passwd)  VALUES('$passwd')";
+   $sql_insert2= "INSERT INTO user (passwd) VALUES('$passwd')";
 
-    if(mysqli_query($link,$sql_insert))
+   $sql="UPDATE user SET userID= concat(userID,ID)";
+
+
+    if(mysqli_query($link,$sql_insert1))
     {
-		header("Location:index.php");
-        echo"<h3> Congratulations! You are now registered</h3>";
-        ///echo "<a href='index.php' class='btn btn-lg btn-success btn-block'>Main Page</a>";
+       // echo"<p> Congratulations! You are now registered</p>";
+       echo"<a href='index.php' class='btn btn-sm btn-success btn-block'>Main Page</a>";
     }
     else {
-        echo "Sorry that did not go through, an error has occured";
-        echo "Please try again.";
+        echo "Sorry that did not go through, an error has occured  ";
+        
+		}
+   if(mysqli_query($link,$sql_insert2))
+    {
+		//header("Location:index.php");
+        echo"<h4> Congratulations! You have now created your account.<br> Take note of your password !</h4>";
+        //echo "<a href='index.php' class='btn btn-sm btn-success btn-block'>Main Page</a>";
     }
+    else {
+        
+		  echo "With your password";
+        }
+        if(mysqli_query($link,$sql))
+        {
+            echo " ";
+         } 
+            else {
+                echo " an error has occured  ";
+                
+                }
+	
     mysqli_close($link);
     ?><!--close PHP-->
     <?php include "footer.php";?> 
