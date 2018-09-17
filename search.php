ac0bafd64065d2538f0aaf13597e724b5fb4ac28
L00149523 <L00149523@student.lyit.ie>
2018-09-17 13:06:20 +0100

    search file used by the admin page

---
+++ search.php
@@ -0,0 +1,47 @@
+ <!--Author@ Nimmy Augustin-->
+ <!--Date@ 15/09/18-->
+ <!--A DevOps group project-->
+ <!--php file used by studentdisplay.php -->
+
+
+<h1>Search by County</h1>
+<form action="search.php" method="post" enctype="multipart/form-data">
+    County:  <input type="text" name="county" value="" placeholder="" size="20">
+    <input type="submit" value="Search" name="submit">
+</form>
+
+<?php 
+
+include_once("config.php");
+
+    if (isset($_POST['county']))
+    {
+        $county = $_POST['county'];
+        
+        $sql = "SELECT * FROM studentdetails WHERE 'county/state' ='$county';";
+        
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+   ?>
+
+   <h3>Search results:</h3>
+
+   <?php
+            while($row = $result->fetch_assoc()) {
+                echo '<table>';
+                echo '<tr><td>UserID:</td><td>'.$row["userID"].'</td></tr>';
+                echo '<tr><td>Firstname:</td><td>'.$row["Fname"].'</td></tr>';
+                echo '<tr><td>Lastname:</td><td>'.$row["Lname"].'</td></tr>';
+                echo '<tr><td>County:</td><td>'.$row["county/state"].'</td></tr>';
+                echo '</table>';
+                echo '<hr />';
+            }
+        }
+        else {
+           echo "0 results";
+        }
+    }
+    include_once("menu.php");
+
+?>
