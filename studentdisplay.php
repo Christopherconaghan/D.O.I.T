6929f0edf921d3550323d95ce942726bfb749a44
L00149523 <L00149523@student.lyit.ie>
2018-09-17 13:06:59 +0100

    main php file used by the admin page

---
+++ studentdisplay.php
@@ -0,0 +1,14 @@
+ <!--Author@ Nimmy Augustin-->
+ <!--Date@ 15/09/18-->
+ <!--A DevOps group project-->
+ <!--A webpage which will allow admins to view enrolled student's details -->
+ 
+<?php include "adminheader.php";?>
+   
+	<div class="container">
+        <h1>Welcome Admin</h1>
+        
+        <h2 style="color:blue;">All Students List:</h2>
+        <?php include_once("display.php"); include_once("menu.php");?>
+	</div>
+    <?php include "footer.php";?>
