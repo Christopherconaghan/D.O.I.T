45fb8e554d33cddae25d6568f36b21fd9044a1e3
L00149523 <41998973+L00149523@users.noreply.github.com>
2018-09-17 22:28:46 +0100

    Update profiles.php
    display of student details and profiles retrieveing data from db.

--- profiles.php
+++ profiles.php
@@ -1,4 +1,4 @@
- <!--Author@ Nimmy Augustin-->
+<!--Author@ Nimmy Augustin-->
  <!--Date@ 15/09/18-->
  <!--A DevOps group project-->
  <!--file used by studentdisplay.php -->
@@ -39,17 +39,29 @@
         
         if ($result->num_rows > 0) {
    
+                echo '<hr />';
+                echo '<table class="table table-striped">';
+                echo '<thead>
+                    <tr>
+                      <th scope="col">User Id</th>
+                      <th scope="col">First Name</th>
+                      <th scope="col">Last Name</th>
+                      <th scope="col">County</th>
+                    </tr>
+                  </thead>';
+                echo '<tbody>';
             while($row = $result->fetch_assoc()) {
                 
-                echo '<hr />';
-                echo '<table>';
-                echo '<tr><td>UserID:</td><td>'.$row["userID"].'</td></tr>';
-                echo '<tr><td>Firstname:</td><td>'.$row["Fname"].'</td></tr>';
-                echo '<tr><td>Lastname:</td><td>'.$row["Lname"].'</td></tr>';
-                echo '<tr><td>County:</td><td>'.$row["county/state"].'</td></tr>';
-                echo '</table>';
+                echo '<tr>';
+                echo '<td>'.$row["userID"].'</td>';
+                echo '<td>'.$row["Fname"].'</td>';
+                echo '<td>'.$row["Lname"].'</td>';
+                echo '<td>'.$row["county/state"].'</td>';
+                echo '</tr>';
                 
             }
+            echo '</tbody>';
+                echo '</table>';
         }
         else {
            echo "0 results";
