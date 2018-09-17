3ac03c55efaf41a02bc0b23a6042f07e590eb87c
L00149523 <L00149523@student.lyit.ie>
2018-09-17 13:04:46 +0100

    menu file used by the admin page

---
+++ menu.php
@@ -0,0 +1,7 @@
+ <!--Author@ Nimmy Augustin-->
+ <!--Date@ 15/09/18-->
+ <!--A DevOps group project-->
+ <!--php file used by studentdisplay.php -->
+
+<hr />
+<a href="index.php">Index</a> | <a href="add.php">Add</a> | <a href="profiles.php">Profiles</a> | <a href="search.php">Search</a>
